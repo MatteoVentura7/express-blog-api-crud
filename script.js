b407b701c1bf89f7c18a1119c060be9cb9de424f
MatteoVentura <matteo.ventura18@gmail.com>
2025-01-22 16:17:01 +0100

    fix

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const postRouter = require('./routers/post.js')
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+// ROUTER
+const postRouter = require("./routers/postRouter");
+const employersRouter = require("./routers/employersRouter");
+//const postRouter = require("./routers/post.js");
 
-app.get('/', (req, res) => {
-  res.send('Server del mio Blog')
-})
+app.use(express.static("public"));
 
-app.use('/post', postRouter)
+app.get("/", (req, res) => {
+  res.send("Server del mio Blog");
+});
+
+// Registro le rotte
+
+app.use("/post", postRouter);
+app.use("/employers", employersRouter);
+// app.use("/post", postRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
