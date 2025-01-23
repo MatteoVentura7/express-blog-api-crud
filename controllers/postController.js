e1e68f908ffaeb1961dd215a6ff36741c59b329e
MatteoVentura <matteo.ventura18@gmail.com>
2025-01-23 15:50:47 +0100

    add : store,uptade,modify

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,15 +20,47 @@ const show = (req, res) => {
 };
 // Store
 const store = (req, res) => {
-  res.send("Creazione nuovo post");
+  const newId = postData[postData.length - 1].id + 1;
+  const newpost = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    image: req.body.image,
+  };
+  postData.push(newpost);
+  res.status(201).json(newpost);
 };
 // Update
 const update = (req, res) => {
-  res.send(`Modifica integrale del post: ${req.params.id}`);
+  const post = postData.find((elm) => elm.id == req.params.id);
+  if (!post) {
+    return res.status(404).json({
+      error: "post not found",
+    });
+  }
+  post.title = req.body.title;
+  post.content = req.body.content;
+  post.image = req.body.image;
+  res.json(post);
 };
 // Modify
 const modify = (req, res) => {
-  res.send(`Modifica parziale del post ${req.params.id}`);
+  const post = postData.find((elm) => elm.id == req.params.id);
+  if (!post) {
+    return res.status(404).json({
+      error: "post not found",
+    });
+  }
+  if (req.body.title) {
+    post.title = req.body.title;
+  }
+  if (req.body.content) {
+    post.content = req.body.content;
+  }
+  if (req.body.image) {
+    post.image = req.body.image;
+  }
+  res.json(post);
 };
 // Delete
 const destroy = (req, res) => {
